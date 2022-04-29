eae5dc1ab207df5b0991c286b89af265a2292254
leobozzi <lbozzi@gmail.com>
2022-04-29 08:43:23 -0300

    add html2text

---
+++ 14.0.debug/make.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sd build --rm=true -t lbozzi/odoo-docker-lb:14.0.debug ./
--- 14.0/make.sh
+++ 14.0/make.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --rm=true -t lbozzi/odoo-docker-lb:14.0 ./
+sd build --rm=true -t lbozzi/odoo-docker-lb:14.0 ./
